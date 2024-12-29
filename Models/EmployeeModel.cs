16f005c1f2dd76c229c9303070ebe5fff2622719
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-12-29 10:10:29 -0300

    feat: employee model and required data created

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiUsers.Enums;
+
+namespace WebApiUsers.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DepartmentEnum Department { get; set; }
+        public bool Active { get; set; }
+        public ShiftEnum Shift { get; set; } 
+        public DateTime CreationDate { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime ChangeDate { get; set; } = DateTime.Now.ToLocalTime();
+    }
+}
