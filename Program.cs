1834254198f4de972c7f8134b669f6c1f510ea33
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-12-31 15:42:21 -0300

    feat: configured SqlServer database

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiUsers.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configurando ApplicationDbContext para ultilizar SqlServer e conectar-se ao DefaultConnection confirurado no appsettings.json
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
